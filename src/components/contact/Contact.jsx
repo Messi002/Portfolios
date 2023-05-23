db9d4a20841f5330e676a3709617f813f3c8b5c4
Messi002 <rostandmessi2@gmail.com>
2023-05-23 11:34:20 +0100

    added services, experience and contact section fully operational

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessageLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_1euh7qj', 'template_8kehksf', form.current, 'vo7OEzUI7novtnb5w')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+
+      e.target.reset();
+  };
   return (
     <section id='contact'>
-      contact
+     <h5>Get In Touch</h5>
+     <h2>Contact Me</h2>
+
+     <div className="container contact__container">
+      {/* CONTACT OPTIONS */}
+      <div className="contact__options">
+        {/* CONTACT OPTION: EMAIL */}
+        <article className="contact__option">
+          <MdOutlineEmail className='contact__option-icon'/>
+          <h4>Email</h4>
+          <h5>rostandmessi2@gmail.com</h5>
+          <a href="mailto:rostandmessi2@gmail.com">Send a Message</a>
+        </article>
+
+          {/* CONTACT OPTION: MESSENGER */}
+          <article className="contact__option">
+          <RiMessageLine className='contact__option-icon'/>
+          <h4>Messenger</h4>
+          <h5>Austin Gareth</h5>
+          <a href="http://m.me/austin.gareth">Send a Message</a>
+        </article>
+
+          {/* CONTACT OPTION: WHATSAPP */}
+          <article className="contact__option">
+          <BsWhatsapp className='contact__option-icon'/>
+          <h4>WhatsApp</h4>
+          <h5>+23765*****35</h5>
+          <a href="https://wa.me/+237653921635">Send a Message</a>
+        </article>
+      </div>
+
+      {/* FORM SECTION */}
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" name="name" placeholder='Your Full Name' required />
+        <input type="email" name="email" placeholder='Your Email' required />
+        <textarea name="message" rows="7" required></textarea>
+        <button type="submit" className='btn btn-primary'>Send Message</button>
+      </form>
+     </div>
     </section>
   )
 }
--- src/components/experience/Expenrience.jsx
+++ src/components/experience/Expenrience.jsx
@@ -1,10 +1,117 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Expenrience = () => {
   return (
     <section id='experience'>
-      Expenrience
+    <h5>What Skills I Have</h5>
+    <h2>My Experience</h2>
+
+    <div className="container experience__container">
+      {/* frontend development */}
+      <div className="experience__frontend">
+        <h3>FrontEnd Development</h3>
+        <div className="experience__content">
+          {/* 1 */}
+          <article className='experience__details'>
+            <BsPatchCheckFill  className='experience__details-icon'/>
+           <div>
+           <h4>HTML</h4>
+            <small className='text-light'>Experienced</small>
+           </div>
+          </article>
+           {/* 2 */}
+           <article className='experience__details'>
+            <BsPatchCheckFill  className='experience__details-icon'/>
+           <div>
+           <h4>CSS</h4>
+            <small className='text-light'>Intermediate</small>
+           </div>
+          </article> 
+          {/* 3 */}
+          <article className='experience__details'>
+            <BsPatchCheckFill  className='experience__details-icon'/>
+           <div>
+           <h4>Javascript</h4>
+            <small className='text-light'>Experienced</small>
+           </div>
+          </article> 
+          {/* 4 */}
+          <article className='experience__details'>
+            <BsPatchCheckFill  className='experience__details-icon'/>
+           <div>
+           <h4>BootStrap</h4>
+            <small className='text-light'>Experienced</small>
+           </div>
+          </article> 
+          {/* 5 */}
+          <article className='experience__details'>
+            <BsPatchCheckFill  className='experience__details-icon'/>
+           <div>
+           <h4>TailWind</h4>
+            <small className='text-light'>Experienced</small>
+           </div>
+          </article>
+                    {/* 6 */}
+          <article className='experience__details'>
+            <BsPatchCheckFill  className='experience__details-icon'/>
+           <div>
+           <h4>React</h4>
+            <small className='text-light'>Experienced</small>
+           </div>
+          </article>
+        </div>
+      </div>
+
+      {/* backend development */}
+      <div className="experience__backend">
+        <h3>Backend Development</h3>
+        <div className="experience__content">
+          {/* 1 */}
+          <article className='experience__details'>
+            <BsPatchCheckFill  className='experience__details-icon'/>
+           <div>
+           <h4>NODE JS</h4>
+            <small className='text-light'>Experienced</small>
+           </div>
+          </article>
+           {/* 2 */}
+           <article className='experience__details'>
+            <BsPatchCheckFill  className='experience__details-icon'/>
+           <div>
+           <h4>MONGODB</h4>
+            <small className='text-light'>Intermediate</small>
+           </div>
+          </article> 
+          {/* 3 */}
+          <article className='experience__details'>
+            <BsPatchCheckFill  className='experience__details-icon'/>
+           <div>
+           <h4>PHP</h4>
+            <small className='text-light'>Experienced</small>
+           </div>
+          </article> 
+          {/* 4 */}
+          <article className='experience__details'>
+            <BsPatchCheckFill  className='experience__details-icon'/>
+          <div>
+          <h4>MYSQL</h4>
+            <small className='text-light'>Experienced</small>
+          </div>
+          </article> 
+          {/* 5 */}
+          <article className='experience__details'>
+            <BsPatchCheckFill className='experience__details-icon'/>
+           <div>
+           <h4>PYTHON</h4>
+            <small className='text-light'>Experienced</small>
+           </div>
+          </article>
+          
+        </div>
+      </div>
+    </div>
     </section>
   )
 }
