f0bd7870517d018fef71d077a41bcc06ddc051ac
Messi002 <rostandmessi2@gmail.com>
2023-05-29 19:15:19 +0100

    added footer, portfolios, testimonails

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,86 @@
 import React from 'react'
 import './portfolio.css'
+import img1 from '../../assets/img/portfolio1.jpg'
+import img2 from '../../assets/img/portfolio2.jpg'
+import img3 from '../../assets/img/portfolio3.jpg'
+import img4 from '../../assets/img/portfolio4.jpg'
+import img5 from '../../assets/img/portfolio5.png'
+import img6 from '../../assets/img/portfolio6.jpg'
 
 const Portfolio = () => {
   return (
-    <div>
-      portfolio
-    </div>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className='container portfolio_container'>
+<article className='portfolio__item'>
+<div className="portfolio__item-img">
+  <img src={img1} alt="" />
+</div>
+<h3>This is a portfolio item title</h3>
+<div className="portfolio__item-cta">
+<a href="http://github.com" className='btn' target='_blank'>Github</a>
+<a href="http://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+</div>
+</article>
+{/*  */}
+<article className='portfolio__item'>
+<div className="portfolio__item-img">
+  <img src={img2} alt="" />
+</div>
+<h3>This is a portfolio item title</h3>
+<div className="portfolio__item-cta">
+<a href="http://github.com" className='btn' target='_blank'>Github</a>
+<a href="http://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+</div>
+</article>
+{/*  */}
+<article className='portfolio__item'>
+<div className="portfolio__item-img">
+  <img src={img3} alt="" />
+</div>
+<h3>This is a portfolio item title</h3>
+<div className="portfolio__item-cta">
+<a href="http://github.com" className='btn' target='_blank'>Github</a>
+<a href="http://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+</div>
+</article>
+{/*  */}
+<article className='portfolio__item'>
+<div className="portfolio__item-img">
+  <img src={img3} alt="" />
+</div>
+<h3>This is a portfolio item title</h3>
+<div className="portfolio__item-cta">
+<a href="http://github.com" className='btn' target='_blank'>Github</a>
+<a href="http://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+</div>
+</article>
+{/*  */}
+<article className='portfolio__item'>
+<div className="portfolio__item-img">
+  <img src={img4} alt="" />
+</div>
+<h3>This is a portfolio item title</h3>
+<div className="portfolio__item-cta">
+<a href="http://github.com" className='btn' target='_blank'>Github</a>
+<a href="http://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+</div>
+</article>
+{/*  */}
+<article className='portfolio__item'>
+<div className="portfolio__item-img">
+  <img src={img5} alt="" />
+</div>
+<h3>This is a portfolio item title</h3>
+<div className="portfolio__item-cta">
+<a href="http://github.com" className='btn' target='_blank'>Github</a>
+<a href="http://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+</div>
+</article>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,12 +1,79 @@
 import React from 'react'
 import './testimonials.css'
+import avt1 from '../../assets/img/avatar1.jpg'
+import avt2 from '../../assets/img/avatar2.jpg'
+import avt3 from '../../assets/img/avatar3.jpg'
+import avt4 from '../../assets/img/avatar4.jpg'
+import { Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: avt1,
+    name: 'Tina Snow',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
+  },
+  {
+    avatar: avt2,
+    name: 'Shatta Wale',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
+  },
+  {
+    avatar: avt3,
+    name: 'Kwame Despite',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
+  },
+  {
+    avatar: avt4,
+    name: 'Louisa McBrown',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
+  },
+
+
+];
 
 const Testimonials = () => {
   return (
     <section id='testimonials'>
-      Testimonials
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper className="container testimonials__container"
+      
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+
+      
+      >
+     
+{
+  data.map(({avatar, name, review}, index) =>{
+    return (
+      <SwiperSlide key={index} className='testimonial'>
+<div className='client__avatar'>
+  <img src={avatar} alt="Avator One" />
+</div>
+<h5>{name}</h5>
+  <small className='client__review'>
+ {review}
+  </small>
+</SwiperSlide>
+    )
+  })
+}
+        
+      </Swiper>
+      
     </section>
   )
 }
 
 export default Testimonials
+
+
