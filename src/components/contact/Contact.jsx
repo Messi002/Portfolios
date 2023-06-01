5c4e8d0559712615ae14d08b437f1bb061cdc3a4
Messi002 <rostandmessi2@gmail.com>
2023-06-01 17:13:24 +0100

    working

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -54,3 +54,16 @@ const Contact = () => {
         </article>
       </div>
 
+      {/* FORM SECTION */}
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" name="name" placeholder='Your Full Name' required />
+        <input type="email" name="email" placeholder='Your Email' required />
+        <textarea name="message" rows="7" required></textarea>
+        <button type="submit" className='btn btn-primary'>Send Message</button>
+      </form>
+     </div>
+    </section>
+  )
+}
+
+export default Contact
