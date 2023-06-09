852dc093cbfff11f730388629f994cfc22e94258
Messi002 <rostandmessi2@gmail.com>
2023-06-09 13:37:04 +0100

    added backend

--- src/components/portfolio/portfolio2.jsx
+++ src/components/portfolio/portfolio2.jsx
@@ -3,32 +3,32 @@ import axios from 'axios';
 import './portfolio.css';
 
 const Portfolio = () => {
-  const [data, setData] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get('https://portfolio-backend-end.cyclic.app/api/portfolio')
-      .then(response => {
-        const formattedData = response.data.data.map(item => ({
-          id: item._id,
-          image: item.image,
-          title: item.title,
-          github: item.github,
-          demo: item.demo
-        }));
-        setData(formattedData);
-      })
-      .catch(error => {
-        console.error('Error fetching portfolio data:', error);
-      });
-  }, []);
+    const [data, setData] = useState([]);
+
+    useEffect(()=> {
+        axios.get("https://portfolio-backend-end.cyclic.app/api/portfolio").then(response => {
+            const formattedData = response.data.data.map(item => ({
+                id:item._id,
+                image: item.image,
+                title: item.title,
+                github: item.github,
+                demo: item.demo
+            }));
+            setData(formattedData)
+        }).catch(error => {
+            console.log("Error fetching portfolio data:", error);
+        })
+    }, [])
 
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className='container portfolio_container'>
-      {data.map(({ id, image, title, github, demo }) => (
+        {
+
+        data.map(({ id, image, title, github, demo }) => (
           <article key={id} className='portfolio__item'>
             <div className="portfolio__item-img">
               <img src={image} alt={title} />
@@ -39,7 +39,9 @@ const Portfolio = () => {
               <a href={demo} className='btn btn-primary' target='_blank' rel='noopener noreferrer'>Live Demo</a>
             </div>
           </article>
-        ))}
+        ))
+        
+        }
       </div>
     </section>
   );
